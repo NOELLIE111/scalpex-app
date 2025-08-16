5f9dbb1fcb69a35690fda96b3d52ae2bcb574050
Kirill <kirovec007@gmail.com>
2025-08-16 20:01:45 +0300

    feat: Add python-dotenv and load API key

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 
 from kivy.app import App
+from dotenv import load_dotenv
+import os
+
+load_dotenv() # Загружает переменные из файла .env
 
 # Создаем класс нашего приложения, наследуясь от App
 class ScalpExApp(App):
@@ -11,8 +15,11 @@ class ScalpExApp(App):
 
     def handle_button_press(self):
         # self.root дает доступ к корневому виджету (нашему BoxLayout)
-        # self.root.ids дает доступ ко всем виджетам внутри, у которых есть id
-        info_label = self.root.ids.info_label
+        # self.root.ids дает доступ ко всем виджетам внутри, у которых есть id        
+        info_label = self.root.ids.info_label # Находим виджет по его id
+        api_key = os.getenv("API_KEY", "Ключ не найден") # Безопасно получаем ключ
+        
+        print(f"Загружен API ключ: {api_key}")
         info_label.text = "Отлично! Текст изменен из Python!"
 
 # Стандартная точка входа в Python-скрипт
